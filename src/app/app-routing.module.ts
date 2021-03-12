3467258532b0994085e920d8d4b2b61b03935e85
adsoftsito <adsoft@live.com.mx>
2021-03-12 17:43:56 -0600

    init colpos template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeCarruselComponent } from './home/home-carrusel/home-carrusel.component';
+import { HomeEcommerceComponent } from './home/home-ecommerce/home-ecommerce.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeCarruselComponent
+  },
+  {
+    path:'ecommerce',
+    component: HomeEcommerceComponent
+  } 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
