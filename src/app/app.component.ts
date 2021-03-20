720376ece23f8940fd67828ea0fa1ab90c4e7f30
adsoftsito <adsoft@live.com.mx>
2021-03-20 16:07:59 -0600

    update header-footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'colpos';
+  admin = false;
+
+  constructor( private route: ActivatedRoute ) { 
+    //this.route.params.subscribe( res =>console.log(res.id)); 
+    var id = this.route.snapshot.queryParamMap.get('id')
+    console.log(
+     id
+    );
+    if (id=='82860')
+    {
+      this.admin = true;
+    }
+
+  }
+
 }
