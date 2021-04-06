d16bc4ead397fd07e94be45735ad46e6af7b768d
DessTEC <A01733753@itesm.mx>
2021-04-05 19:34:08 -0500

    Editar app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home/home.component';
 import { HomeNavbarComponent } from './home/home-navbar/home-navbar.component';
@@ -49,7 +50,6 @@ import { ForoPostNuevoComponent } from './foros/foro-post-nuevo/foro-post-nuevo.
 import { SecurityPerfilInformacionComponent } from './security/security-perfil-informacion/security-perfil-informacion.component';
 import { SecurityPerfilEditarComponent } from './security/security-perfil-editar/security-perfil-editar.component';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
